797e3d921392ae8bcb9060c7ecff2efe3a34960d
basuatrayee <basuatrayee@gmail.com>
2016-12-18 22:35:06 -1000

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -4,7 +4,6 @@ subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 #str(subSetData)
 globalActivePower <- as.numeric(subsetdata$Global_active_power)
-png("plot3.png", width=480, height=480)
 datetime <- strptime(paste(subsetdata$Date, subsetdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 submetering1 <- as.numeric(subsetdata$Sub_metering_1)
 submetering2 <- as.numeric(subsetdata$Sub_metering_2)
